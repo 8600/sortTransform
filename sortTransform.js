c81e68ee16c40f7a6ef04be2e0d5dfdfa30a6eb9
PUGE <100284685@qq.com>
2018-05-15 12:07:30 +0800

    start

---
+++ sortTransform.js
@@ -0,0 +1,33 @@
+const list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', ' U', ' V', ' W', ' X', ' Y', 'Z']
+const obj = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+  H: 7,
+  I: 8,
+  J: 9,
+  K: 10,
+  L: 11,
+  M: 12,
+  N: 13,
+  O: 14,
+  P: 15,
+  Q: 16,
+  R: 17,
+  S: 18,
+  T: 19,
+  U: 20,
+  V: 21,
+  W: 22,
+  X: 23,
+  Y: 24,
+  Z: 25
+}
+// 输入字母排序其实字母和结束字母返回中间列表
+function betwon (start, end, startNum = 0, endNum = 0) {
+  return list.slice(obj[start.toUpperCase()] + startNum, obj[end.toUpperCase()] + startNum)
+}
